de2ef1047481a7a1613ce553eb053c70f59037ba
dirktolsoniii <160078826+dirktolsoniii@users.noreply.github.com>
2026-08-10 01:25:02 -0700

    Update turn_in.py
    Changed Course ID and Programming Assignment ID

--- lab/lab10/turn_in.py
+++ lab/lab10/turn_in.py
@@ -9,8 +9,8 @@ from os.path import isfile, join
 
 # Change these for each assignment
 ASSIGNMENT_RMD_PATH = "lab10.Rmd"
-COURSE_ID = 1223977
-PROGRAMMING_ASSIGNMENT_ID = 7970989
+COURSE_ID = 1324055
+PROGRAMMING_ASSIGNMENT_ID = 8297931
 
 # Don't change these
 PDF_PATH = ASSIGNMENT_RMD_PATH.replace('.Rmd', '.pdf')
